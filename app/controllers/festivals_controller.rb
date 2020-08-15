67f0e0aa3cf156866c44178f778a613140d3c48a
dbpatnodee <dbpatnode@hotmail.com>
2020-08-14 17:27:28 -0700

    festival show and index pages

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,2 +1,19 @@
 class FestivalsController < ApplicationController
+    def index
+        @festivals = Festival.all
+    end
+    
+    def show
+        # byebug
+        @festival = Festival.find(params[:id])
+    end
+
+
+
+    private
+
+    # def festival_params
+    #     params.require(:festival).permit
+    # end
+
 end
