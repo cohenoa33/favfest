dde6714f0be3a98d225cdb1713046c8118beccd4
dbpatnodee <dbpatnode@hotmail.com>
2020-08-14 18:02:12 -0700

    added content to show &index for artists & festivals

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :organizers
-  resources :artist_festivals
-  resources :artists
-  resources :favorites
-  resources :festivals
-  resources :fans
+  # resources :organizers
+  # resources :artist_festivals
+  resources :artists, only: [:index, :show]
+  # resources :favorites
+  resources :festivals, only: [:index, :show]
+  # resources :fans
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
